6315731277109410d334753dcb731f7a5511fe97
MargotVanO <49273193+MargotVanO@users.noreply.github.com>
2019-05-02 11:09:37 +0200

    editor maakt shit mooi

--- classes/post.class.php
+++ classes/post.class.php
@@ -1,17 +1,19 @@
 <?php
-include_once('db.class.php');
+
+include_once 'db.class.php';
 
 class Post
-{   public $userId;
+{
+    public $userId;
     public $image;
     public $description;
     public $postId;
-    
+
     public function getUserId()
-        {
-                return $this->userId;
-        }
-    
+    {
+        return $this->userId;
+    }
+
     public function setUserId($userId)
     {
         $this->userId = $userId;
@@ -20,10 +22,10 @@ class Post
     }
 
     public function getImage()
-        {
-                return $this->image;
-        }
-    
+    {
+        return $this->image;
+    }
+
     public function setImage($image)
     {
         $this->image = $image;
@@ -32,10 +34,10 @@ class Post
     }
 
     public function getDescription()
-        {
-                return $this->description;
-        }
-    
+    {
+        return $this->description;
+    }
+
     public function setDescription($description)
     {
         $this->description = $description;
@@ -44,18 +46,18 @@ class Post
     }
 
     /**
-     * Get the value of postId
-     */ 
+     * Get the value of postId.
+     */
     public function getPostId()
     {
         return $this->postId;
     }
 
     /**
-     * Set the value of postId
+     * Set the value of postId.
      *
-     * @return  self
-     */ 
+     * @return self
+     */
     public function setPostId($postId)
     {
         $this->postId = $postId;
@@ -63,19 +65,22 @@ class Post
         return $this;
     }
 
-    public function newPost(){
+    public function newPost()
+    {
         $conn = Db::getInstance();
-        $statement = $conn->prepare("INSERT INTO posts (user_id, description, image, date_created) values (:userid, :description, :image, NOW())");
-        $statement->bindValue(":userid", $this->getUserId());
-        $statement->bindValue(":image", $this->getImage());
-        $statement->bindValue(":description", $this->getDescription());
-        return $statement->execute();
+        $statement = $conn->prepare('INSERT INTO posts (user_id, description, image, date_created) values (:userid, :description, :image, NOW())');
+        $statement->bindValue(':userid', $this->getUserId());
+        $statement->bindValue(':image', $this->getImage());
+        $statement->bindValue(':description', $this->getDescription());
 
+        return $statement->execute();
     }
 
-    public static function getAll(){
+    public static function getAll()
+    {
         $conn = Db::getInstance();
-        $result = $conn->query("SELECT posts.*,users.firstname,users.lastname FROM posts,users WHERE posts.user_id=users.id ");
+        $result = $conn->query('SELECT posts.*,users.firstname,users.lastname FROM posts,users WHERE posts.user_id=users.id ');
+
         return $result->fetchAll(PDO::FETCH_CLASS, __CLASS__);
     }
 
@@ -86,17 +91,15 @@ class Post
     //     return $result=$statement->fetch(PDO::FETCH_ASSOC);
     // }
 
-    public static function getThisPost(){
+    public static function getThisPost($id)
+    {
         try {
-        $conn = Db::getInstance();
-        $statement = $conn->prepare("SELECT posts.*,users.firstname,users.lastname,users.picture FROM posts,users WHERE posts.id=:id AND posts.user_id=users.id");
-        $statement->bindValue(":id", $_GET['id']);
-        var_dump($statement->fetchAll());
-        }
-        catch (Expection $e) {
-            echo "sorry, not working";
+            $conn = Db::getInstance();
+            $statement = $conn->prepare('SELECT posts.*,users.firstname,users.lastname,users.picture FROM posts,users WHERE posts.id=:id AND posts.user_id=users.id ');
+            $statement->bindValue(':id', $id);
+            var_dump($statement->fetch(PDO::FETCH_OBJ));
+        } catch (Expection $e) {
+            echo 'sorry, not working';
         }
     }
-
 }
-?>
