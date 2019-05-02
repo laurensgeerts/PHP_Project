30fd770e32bc5a5f5d1b9e10ae44d1c727cae827
MargotVanO <49273193+MargotVanO@users.noreply.github.com>
2019-05-02 10:14:40 +0200

    Update detail.php

--- detail.php
+++ detail.php
@@ -3,30 +3,36 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$id=$_GET['id'];
+$id = $_GET['id'];
 
 session_start();
-if( $_SESSION['loggedin'] == false){
-  header('Location: login.php');
-};
+if ($_SESSION['loggedin'] == false) {
+    header('Location: login.php');
+}
 
-include_once("classes/user.class.php");
+include_once 'classes/user.class.php';
 $user = new User();
-$user->setUser_id($_SESSION["user_id"]);
+$user->setUser_id($_SESSION['user_id']);
 $profile = $user->getUserInfo();
 
-//get post --> display image, description, date, name poster, image poster 
-include_once("classes/post.class.php");
-$post=Post::getThisPost($id);
+//get post --> display image, description, date, name poster, image poster
+include_once 'classes/post.class.php';
+$post = Post::getThisPost($id);
 
-include_once("classes/comment.class.php");
-if(!empty($_POST)){
-    $comment=new Comment;
-    $comment->setUserId($_SESSION["user_id"]);
-    $comment->setPostId($id);
-    $comment->setComment($_POST['comment']);
-    $comment->newComment();
+include_once 'ajax/postcomment.php';
+include_once 'classes/comment.class.php';
+if (!empty($_POST)) {
+    try {
+        $comment = new Comment();
+        $comment->setUserId($_SESSION['user_id']);
+        $comment->setPostId($id);
+        $comment->setComment($_POST['comment']);
+        $comment->newComment();
+    } catch (\Throwable $th) {
+        //throw $th;
+    }
 }
+
 //$comments=Comment::getAll();
 $comments = new Comment();
 
@@ -42,30 +48,56 @@ $comments = new Comment();
     <title>post <?php echo $id; ?></title>
 </head>
 <body>
-    <?php include_once("nav.inc.php"); ?>
-        <!-- <img src="<?php //echo $post->image; ?>" alt="">
+    <?php include_once 'nav.inc.php'; ?>
+        <!-- <img src="<?php //echo $post->image;?>" alt="">
         <div class="textOfPost">
-            <img src="<?php //echo $post->picture; ?>" class="profilepic">
+            <img src="<?php //echo $post->picture;?>" class="profilepic">
             <p> <?php //echo $post->firstname." ".$post->lastname;?> </p>
-            <p> <?php //echo $post->date_created; ?> </p>
-            <p> <?php //echo $post->description; ?> </p>
+            <p> <?php //echo $post->date_created;?> </p>
+            <p> <?php //echo $post->description;?> </p>
         </div> -->
         <img src="<?php echo $post['image']; ?>" alt="">
         <div class="textOfPost">
             <img src="<?php echo $post['picture']; ?>" class="profilepic">
-            <p> <?php echo $post['firstname']." ".$post['lastname'];?> </p>
+            <p> <?php echo $post['firstname'].' '.$post['lastname']; ?> </p>
             <p> <?php echo $post['date_created']; ?> </p>
             <p> <?php echo $post['description']; ?> </p>
         </div>
-    <form method="post" enctype="multipart/form-data">
+    <form method="post" enctype="">
         <input type="text" name="comment" id="comment" placeholder="write something nice">
+        <input id="btnSubmit" type="submit" value="Add comment" />
     </form>
-    <?php foreach($comments as $comment): ?>
+    <?php foreach ($comments as $comment): ?>
         <div class="comment">
-            <p> <?php echo $comment->firstname." ".$comment->lastname;?> </p>
-            <p> <?php echo $comment->date_created; ?> </p>
-            <p> <?php echo $comment->comment; ?> </p>
+            <p> <?php echo $comment['firstname'].' '.$comment['lastname']; ?> </p>
+            <p> <?php echo $comment['date_created']; ?> </p>
+            <p> <?php echo $comment['comment']; ?> </p>
         </div>
-    <?php endforeach;?>
+    <?php endforeach; ?>
+    <script
+	    src="https://code.jquery.com/jquery-3.3.1.min.js"
+	    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+	    crossorigin="anonymous">
+    </script>
+    <script>
+	    $("#btnSubmit").on("click",function(e){
+		    var text = $("#comment").val(); //waarde van de text input
+		    //console.log(text);
+
+		    $.ajax({
+  			    method: "POST",
+  			    url: "ajax/postcomment.php",
+ 			    data: { comment: comment },
+			    dataType: 'json'
+		    })
+  		    .done(function( res ) {
+    		    //alert( "Data Saved: " + msg );
+			    if(res.status == 'succes'){
+				
+			}
+  		    });
+		    e.preventDefault();
+	    });
+    </script>
 </body>
 </html>
