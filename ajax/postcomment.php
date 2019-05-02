30d52595cd7fb8ae1a42a9b2d242001ba669e2f2
MargotVanO <49273193+MargotVanO@users.noreply.github.com>
2019-05-02 11:08:06 +0200

    ajax werkt

--- ajax/postcomment.php
+++ ajax/postcomment.php
@@ -1,28 +1,27 @@
 <?php
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include_once('../classes/comment.class.php');
+include_once './classes/comment.class.php';
 
-if(!empty($_POST)){
-
-    try{
-        $comment=new Comment;
-        $comment->setUserId($_SESSION["user_id"]);
+if (!empty($_POST)) {
+    try {
+        $comment = new Comment();
+        $comment->setUserId($_SESSION['user_id']);
         $comment->setPostId($id);
         $comment->setComment($_POST['comment']);
         $comment->newComment();
 
-        $result =[
-            "status"=> "succes",
-            "message"=>"ya did it"
+        $result = [
+            'status' => 'succes',
+            'message' => 'ya did it',
         ];
-
-    } catch(trowable $t){
-        $result =[
-            "status"=> "failed",
-            "message"=>"ya didn't it"
+    } catch (trowable $t) {
+        $result = [
+            'status' => 'failed',
+            'message' => "ya didn't it",
         ];
     }
     echo json_encode($result);
@@ -30,5 +29,3 @@ if(!empty($_POST)){
     //query (insert)
     //antwoord geven aan JS frontend (json)
 }
-
-?>
