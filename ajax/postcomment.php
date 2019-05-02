925f042c2dd8315f6333caf9153ea03084791a17
MargotVanO <49273193+MargotVanO@users.noreply.github.com>
2019-05-02 10:14:23 +0200

    added ajax.. include doesn't work

---
+++ ajax/postcomment.php
@@ -0,0 +1,34 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include_once('../classes/comment.class.php');
+
+if(!empty($_POST)){
+
+    try{
+        $comment=new Comment;
+        $comment->setUserId($_SESSION["user_id"]);
+        $comment->setPostId($id);
+        $comment->setComment($_POST['comment']);
+        $comment->newComment();
+
+        $result =[
+            "status"=> "succes",
+            "message"=>"ya did it"
+        ];
+
+    } catch(trowable $t){
+        $result =[
+            "status"=> "failed",
+            "message"=>"ya didn't it"
+        ];
+    }
+    echo json_encode($result);
+    //connectie
+    //query (insert)
+    //antwoord geven aan JS frontend (json)
+}
+
+?>
