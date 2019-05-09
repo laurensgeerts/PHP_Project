6c9547bc98c9d50dadc121adcf05c34cc39031cc
Laurens Geerts <geertslaurens@gmail.com>
2019-05-09 10:45:45 +0200

    feature 5 gefixt

--- classes/post.class.php
+++ classes/post.class.php
@@ -87,11 +87,14 @@ class Post
         WHERE
         posts.user_id IN 
             (
-            SELECT follow_to FROM followers WHERE follow_from = 5
+            SELECT follow_to FROM followers WHERE follow_from = '.$UsId.'
             /* Fetchen met $UsId werkt nog niet */
             )
         ORDER BY posts.date_created desc
+        LIMIT 20
 ');
+        echo $UsId;
+        $result->execute();
 
         return $result->fetchAll(PDO::FETCH_CLASS, __CLASS__);
     }
